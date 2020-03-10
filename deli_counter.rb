b6381cc397f2e7f1ff7fc7e3fe84897dd1201772
Sean Melton <seanlape@gmail.com>
2020-03-10 18:16:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,8 +16,7 @@ line(katz_deli)
 
 def line_simple(array) # this one follows the same mechanics as learn.co
   current_line = "The simple line is currently:"
-  array.each.with_index(1) do |value, indexemus|  
-  # "each.with_index" is the method...must use "index"
+  array.each.with_index(1) do |value, indexemus|
     current_line << " #{indexemus}. #{value},"    
   end 
   puts current_line
